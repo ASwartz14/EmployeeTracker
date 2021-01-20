97ab203efb3171b9bb4ac0e758b37f4fdefec91f
ASwartz14 <asriley144@gmail.com>
2021-01-19 16:01:53 -0800

    finished questions and queries

---
+++ README.md
@@ -0,0 +1,50 @@
+# Employee Tracker
+
+Command Line Employee Tracker
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#test)
+- [License](#license)
+- [Questions](#questions)
+
+## Description
+
+An employee management system. I used inquirer to prompt questions to be answered about employees, roles, and departments. Use to keep track of names, ids, roles, and salaries. Still need to add update and delete functions.
+
+- See the project repository: https://github.com/ASwartz14/EmployeeTracker on GitHub.
+
+## Visuals
+
+![EmployeeTracker]
+
+## Installation
+
+Clone the repo into your own device. Make a dotenv file or modify server connection to fit your needs. Run the schema.sql file in your MySQL workbench. Make sure to run npm i in your command line. Run node app.js when you're ready to start the program.
+
+## Usage
+
+As a busy business owner I want to easily keep track of my employees and their information.
+
+## Contributing
+
+I built this using chalk, inquirer, mysql, and console table. It is open source, feel free to fork and change/use for yourself or feel free to contact me, I'm happy to colaborate.
+
+## Tests
+
+no testing on this project
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Questions
+
+- Please reach out with any questions.
+  **GitHub**: [https://github.com/ASwartz14](https://github.com/ASwartz14)
+  **Email**:asriley144@gmail.com
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const cTable = require("console.table");
+const chalk = require("chalk");
 // set connection via dotenv so info is protected, start and log server
 const connection = mysql.createConnection({
   host: "localhost",
@@ -13,29 +14,45 @@ const connection = mysql.createConnection({
 });
 connection.connect(function (err) {
   if (err) throw err;
-
+  // ASCII Text
   console.log(
-    "  ______                 _                         _______             _              "
+    chalk.magenta.bold(
+      "  ______                 _                         _______             _              "
+    )
   );
   console.log(
-    " |  ____|               | |                       |__   __|           | |             "
+    chalk.magenta.bold(
+      " |  ____|               | |                       |__   __|           | |             "
+    )
   );
   console.log(
-    " | |__   _ __ ___  _ __ | | ___  _   _  ___  ___     | |_ __ __ _  ___| | _____ _ __  "
+    chalk.magenta.bold(
+      " | |__   _ __ ___  _ __ | | ___  _   _  ___  ___     | |_ __ __ _  ___| | _____ _ __  "
+    )
   );
   console.log(
-    " |  __| | '_ ` _ | '_ | |/ _ | | | |/ _ / _     | | '__/ _` |/ __| |/ / _  '__| "
+    chalk.magenta.bold(
+      " |  __| | '_ ` _ | '_ | |/ _ | | | |/ _ / _     | | '__/ _` |/ __| |/ / _  '__| "
+    )
   );
   console.log(
-    " | |____| | | | | | |_) | | (_) | |_| |  __/  __/    | | | | (_| | (__|   <  __/ |   "
+    chalk.magenta.bold(
+      " | |____| | | | | | |_) | | (_) | |_| |  __/  __/    | | | | (_| | (__|   <  __/ |   "
+    )
   );
   console.log(
-    " |______|_| |_| |_| .__/|_|___/ __, |___|___|    |_|_|  __,_|___|_|____|_|  "
+    chalk.magenta.bold(
+      " |______|_| |_| |_| .__/|_|___/ __, |___|___|    |_|_|  __,_|___|_|____|_|  "
+    )
   );
   console.log(
-    "                   | |             __/ |                                               "
+    chalk.magenta.bold(
+      "                   | |             __/ |                                               "
+    )
   );
-  console.log("                   |_|            |___/     ");
+  console.log(
+    chalk.magenta.bold("                   |_|            |___/     ")
+  );
 
   init();
 });
@@ -53,13 +70,11 @@ function init() {
         "View Departments",
         "View Roles",
         "View Employees",
-        "Update Employee",
-        "Delete an Employee",
         "Exit",
       ],
     })
-    .then((choice) => {
-      switch (choice.choose) {
+    .then((answer) => {
+      switch (answer.choose) {
         case "Add a Department":
           addDepartment();
           break;
@@ -84,23 +99,213 @@ function init() {
           viewEmps();
           break;
 
-        case "Update Employee":
-          updateEmp();
-          break;
-        case "Delete Employee":
-          deleteEmp();
-          break;
         case "Exit":
           connection.end();
           break;
       }
     });
 }
-function addDepartment() {}
-function addRoll() {}
-function addEmployee() {}
-function viewDepartment() {}
-function viewRoles() {}
-function viewEmps() {}
-function updateEmp() {}
-function deleteEmp() {}
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "addDepartment",
+        message: "Please enter the department you would like to add.",
+        validate: function (addDepartment) {
+          if (addDepartment === "") {
+            console.log(
+              chalk.magenta.bold("\nPlease enter a valid department name.")
+            );
+            return false;
+          } else {
+            return true;
+          }
+        },
+      },
+    ])
+    .then(function (answer) {
+      connection.query(
+        "INSERT INTO department SET?",
+        {
+          depName: answer.addDepartment,
+        },
+        function (err) {
+          if (err) throw err;
+          console.log(chalk.magenta.bold("\nNew department added."));
+          init();
+        }
+      );
+    });
+}
+function addRoll() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "addRoll",
+        message: "Please enter the role you would like to add.",
+        validate: function (addRoll) {
+          if (addRoll === "") {
+            console.log(chalk.magenta.bold("\nPlease enter a valid role."));
+            return false;
+          } else {
+            return true;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "Please enter the salary for this role.",
+        validate: function (salary) {
+          if (isNaN(salary) || salary === "") {
+            console.log(chalk.magenta.bold("\nPlease enter a valid salary."));
+            return false;
+          } else {
+            return true;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "depId",
+        message: "Please enter the department ID for this role.",
+        validate: function (depId) {
+          if (isNaN(depId) || depId === "") {
+            console.log(chalk.magenta.bold("\nPlease enter a valid ID."));
+            return false;
+          } else {
+            return true;
+          }
+        },
+      },
+    ])
+    .then(function (answer) {
+      connection.query(
+        "INSERT INTO roles SET?",
+        {
+          title: answer.addRoll,
+          salary: answer.salary,
+          depId: answer.depId,
+        },
+        function (err) {
+          if (err) throw err;
+          console.log(chalk.magenta.bold("\nNew role added."));
+          init();
+        }
+      );
+    });
+}
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "addFirst",
+        message: "Please enter the employee's first name.",
+        validate: function (addFirst) {
+          if (addFirst === "") {
+            console.log(chalk.magenta.bold("\nPlease enter a valid name."));
+            return false;
+          } else {
+            return true;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "addLast",
+        message: "Please enter the employee's last name.",
+        validate: function (addLast) {
+          if (addLast === "") {
+            console.log(chalk.magenta.bold("\nPlease enter a valid name."));
+            return false;
+          } else {
+            return true;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "roleId",
+        message: "Please enter the role ID for this employee.",
+        validate: function (roleId) {
+          if (isNaN(roleId) || roleId === "") {
+            console.log(chalk.magenta.bold("\nPlease enter a valid ID."));
+            return false;
+          } else {
+            return true;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "Please enter the manager's ID for this employee.",
+        validate: function (managerId) {
+          if (isNaN(managerId) || managerId === "") {
+            console.log(chalk.magenta.bold("\nPlease enter a valid ID."));
+            return false;
+          } else {
+            return true;
+          }
+        },
+      },
+    ])
+    .then(function (answer) {
+      connection.query(
+        "INSERT INTO employees SET?",
+        {
+          firstName: answer.addFirst,
+          lastName: answer.addLast,
+          roleId: answer.roleId,
+          managerId: answer.managerId,
+        },
+        function (err) {
+          if (err) throw err;
+          console.log(chalk.magenta.bold("\n New Employee added."));
+          init();
+        }
+      );
+    });
+}
+function viewDepartment() {
+  connection.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    console.log(
+      chalk.magenta.bold(
+        "\n Here is a list all the departments in the database. \n"
+      )
+    );
+    console.table(res);
+
+    init();
+  });
+}
+function viewRoles() {
+  connection.query("SELECT * FROM roles", function (err, res) {
+    if (err) throw err;
+    console.log(
+      chalk.magenta.bold(
+        "\n Here is a list of all the roles in the database. \n"
+      )
+    );
+    console.table(res);
+
+    init();
+  });
+}
+function viewEmps() {
+  connection.query("SELECT * FROM employees", function (err, res) {
+    if (err) throw err;
+    console.log(
+      chalk.magenta.bold(
+        "\n Here is a list of all the employees in the database. \n"
+      )
+    );
+    console.table(res);
+
+    init();
+  });
+}
